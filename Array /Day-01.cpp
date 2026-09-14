93694702133884be306507c4a0bd130f105fc83a
Himanshu Shekhar <chulbul7061@gmail.com>
2026-09-14 23:52:07 +0530

    DayOne with array

---
+++ Array /Day-01.cpp
@@ -0,0 +1,29 @@
+/*
+Array Declararion :
+
+Method 1
+
+int arr[5]={23,4,45,11,5}
+
+Method 2
+
+int arr[]={23,422,55,78,86}
+
+Method 3
+
+int arr[5]={12,22} ---> in this case rest 3 block me garbage value ho ga
+
+Method 4
+
+int arr[4]={0} --> in this case the all value should be {0,0,0,0}
+
+Method 5
+
+int arr[];
+
+now the take the size of array as input then enter the value
+int size;
+{9,34,54,242,77,88}
+
+
+*/
